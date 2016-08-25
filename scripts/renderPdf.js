a76c0d752fded75b1bbb5ee9d39206f71a1c2dc9
AMangano <andreaman87@gmail.com>
2016-08-25 09:27:48 +0200

    Added config parameter to render function

--- scripts/renderPdf.js
+++ scripts/renderPdf.js
@@ -1,13 +1,29 @@
 var phantom = require('phantom');
-
+var extend = require('extend');
 // TODO: make function more generic passing config parameter
 
-var render = function(url, filename, reportData) {
+var render = function(url, filename, reportData, config) {
 
   // Variables
   var sitepage = null;
   var phInstance = null;
   var reportData = reportData;
+
+  var defaultConfig = {
+    viewportSize: {
+      width: 1024,
+      height: 768
+    },
+    render: {
+      format: 'pdf',
+      quality: '100'
+    },
+    // Change timeout as required to allow sufficient time 
+    renderTime: 2000
+  }
+
+  // Merge defaultConfig with function parameter config (if it exists)
+  var config = config ? extend(defaultConfig, config) : defaultConfig;
 
   // Create the Phantom instance
   phantom.create()
@@ -28,16 +44,17 @@ var render = function(url, filename, reportData) {
 
     // TODO: set the layout properties
     // Set the layou properties
-    sitepage.property('viewportSize', {
-      width: 1024,
-      height: 768
-    });
+    sitepage.property('viewportSize', config.viewportSize);
 
     // Doesn't actually open any page
     sitepage.setContent("", url);
 
     sitepage.evaluate(function(reportData) {
+
+      // Clear localStorage
       localStorage.clear();
+
+      // Populate localstorage with reportData
       localStorage.setItem("reportData", reportData);
     }, reportData);
 
@@ -48,17 +65,19 @@ var render = function(url, filename, reportData) {
 
     // Check the status
     if (status !== 'success') {
-
       //console.log('Unable to load the address!');
       phantom.exit();
     } else {
 
       setTimeout(function () {
 
-        sitepage.render(filename, {format: 'pdf', quality: '100'});
+        // Render page with render config
+        sitepage.render(filename, config.render);
+
+        // Close streams
         sitepage.close();
         phInstance.exit();
-      }, 2000); // Change timeout as required to allow sufficient time 
+      }, config.renderTime); 
     }
   });
   // .catch(error => {
